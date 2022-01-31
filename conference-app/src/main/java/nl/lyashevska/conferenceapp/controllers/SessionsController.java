dbc8c84d01dc0f4881f702b1d942db1f7c3ea6d7
lyashevska <olga@herenstraat.nl>
2022-01-31 20:22:43 +0000

    added REST controllers

--- conference-app/src/main/java/nl/lyashevska/conferenceapp/controllers/SessionsController.java
+++ conference-app/src/main/java/nl/lyashevska/conferenceapp/controllers/SessionsController.java
@@ -2,12 +2,16 @@ package nl.lyashevska.conferenceapp.controllers;
 
 import nl.lyashevska.conferenceapp.models.Session;
 import nl.lyashevska.conferenceapp.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Full CRUD controller for the session
+ */
 @RestController
 @RequestMapping("/api/v1/sessions")
 public class SessionsController {
@@ -15,11 +19,14 @@ public class SessionsController {
     @Autowired
     private SessionRepository sessionRepository;
 
+    // list
     @GetMapping
     public List<Session> list(){
         return sessionRepository.findAll();
     }
 
+    // get
+    // require http get
     @GetMapping
     @RequestMapping("{id}")
     public Session get(@PathVariable Long id){
@@ -27,10 +34,35 @@ public class SessionsController {
         return sessionRepository.getById(id);
     }
 
-//    @PostMapping
-//    // @ResponseStatus(HttpStatus.CREATED) // this maps to 201, othervise to 200
-//    public Session create(@RequestBody final Session session){
-//        return sessionRepository.saveAndFlush(session);
-//    }
+    // create
+    // require http post
+    @PostMapping
+    // @ResponseStatus(HttpStatus.CREATED) // this maps to 201, othervise to 200
+    // take all attributes in json payload and auto marshal to session object
+    // then pass to a session repo
+    public Session create(@RequestBody final Session session){
+        return sessionRepository.saveAndFlush(session);
+    }
 
+    // delete endpoint
+    // http verb delete is present
+    @RequestMapping(value = "{id}, method = RequestMethod.DELETE")
+    public void delete(@PathVariable Long id){
+        // also need to check for children recodrs before deleting
+        sessionRepository.deleteById(id);
+    }
+
+    // update
+    // session record
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session){
+        // because this is PUT we axpect all attributes to be passed in, othervise they get 0
+        // A PATCH would only update what you need to update
+        // TODO: Add validation that all attributed are passed in, othervise return a 400 bad payload
+        // find existing record from url
+        Session existingSession = sessionRepository.getById(id);
+        // takes the existing session and copies data onto it, except for session_id
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
 }
--- conference-app/src/main/java/nl/lyashevska/conferenceapp/controllers/SpeakersController.java
+++ conference-app/src/main/java/nl/lyashevska/conferenceapp/controllers/SpeakersController.java
@@ -1,7 +1,9 @@
 package nl.lyashevska.conferenceapp.controllers;
 
+import nl.lyashevska.conferenceapp.models.Session;
 import nl.lyashevska.conferenceapp.models.Speaker;
 import nl.lyashevska.conferenceapp.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,7 @@ import java.util.List;
 public class SpeakersController {
 
     // give CRUD access
+    // create an instance of the session repo and add to the class
     @Autowired
     private SpeakerRepository speakerRepository;
 
@@ -22,16 +25,38 @@ public class SpeakersController {
     }
 
     // return specific speaker
+    // http get mapping
     @GetMapping
     @RequestMapping("{id}")
     public Speaker get(@PathVariable Long id){
         return speakerRepository.getById(id);
     }
 
-//    @PostMapping
-//    public Speaker create(@RequestBody final Speaker speaker){
-//        return speakerRepository.saveAndFlush(speaker);
-//    }
+    // create
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
 
+    // update
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+        // because this is PUT we axpect all attributes to be passed in, othervise they get 0
+        // A PATCH would only update what you need to update
+        // TODO: Add validation that all attributed are passed in, othervise return a 400 bad payload
+        // find existing record from url
+        Speaker existingSpeaker = speakerRepository.getById(id);
+        // takes the existing session and copies data onto it, except for speaker_id
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+
+    // delete endpoint
+    // http verb delete is present
+    @RequestMapping(value = "{id}, method = RequestMethod.DELETE")
+    public void delete(@PathVariable Long id){
+        // also need to check for children recodrs before deleting
+        speakerRepository.deleteById(id);
+    }
 
 }
