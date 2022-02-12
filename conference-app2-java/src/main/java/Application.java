1fcf698888221287f3c927b1e5c03d0a18a60663
lyashevska <olga@herenstraat.nl>
2022-02-12 20:15:38 +0000

    added env var

---
+++ conference-app2-java/src/main/java/Application.java
@@ -0,0 +1,22 @@
+import org.example.service.SpeakerService;
+import org.example.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // SpeakerService service = new SpeakerServiceImpl();
+
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+
+        System.out.println(service);
+
+        System.out.println(service.findAll().get(0).getFirstName());
+
+        SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
+
+        System.out.println(service2);
+    }
+}
